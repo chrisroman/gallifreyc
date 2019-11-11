aa5c2592233fbb8169fbcb79e676dd25136890fe
Christopher Roman <cr469@cornell.edu>
2019-11-11 04:08:36 -0500

    Changed all gallifreyc.types files to extend JL7 stuff

--- compiler/src/gallifreyc/types/GallifreyTypeSystem.java
+++ compiler/src/gallifreyc/types/GallifreyTypeSystem.java
@@ -1,7 +1,8 @@
 package gallifreyc.types;
 
+import polyglot.ext.jl7.types.JL7TypeSystem;
 import polyglot.types.*;
 
-public interface GallifreyTypeSystem extends TypeSystem {
+public interface GallifreyTypeSystem extends JL7TypeSystem {
     // TODO: declare any new methods needed
 }
--- compiler/src/gallifreyc/types/GallifreyTypeSystem_c.java
+++ compiler/src/gallifreyc/types/GallifreyTypeSystem_c.java
@@ -1,8 +1,9 @@
 package gallifreyc.types;
 
+import polyglot.ext.jl7.types.JL7TypeSystem_c;
 import polyglot.types.*;
 
-public class GallifreyTypeSystem_c extends TypeSystem_c implements GallifreyTypeSystem {
+public class GallifreyTypeSystem_c extends JL7TypeSystem_c implements GallifreyTypeSystem {
     // TODO: implement new methods in GallifreyTypeSystem.
     // TODO: override methods as needed from TypeSystem_c.
 }
